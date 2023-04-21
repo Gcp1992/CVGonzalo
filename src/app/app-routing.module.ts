d882e77e85e9087af67bbf95ae3b2b7cf827a456
Gcp1992 <gonzalo.delacruz@softtek.com>
2023-04-21 20:12:35 +0200

    CVGonzalo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { BlogsComponent } from './blogs/blogs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IndexComponent},
+  {path: 'blog', component: BlogsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { Art1Component } from './art1/art1.component';
+import { Art2Component } from './art2/art2.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    BlogsComponent,
+    Art1Component,
+    Art2Component
   ],
   imports: [
     BrowserModule,
